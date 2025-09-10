4b3ddd0e686fa15c278b74b1604cd12baa147480
Amr Rizk <amrelseady65@gmail.com>
2025-09-10 22:26:02 +0300

    task/15146 : add post endpoint to creat doctors

---
+++ ITI.HMS/Requestes/CreatDoctorRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ITI.HMS.Requestes
+{
+    public class CreatDoctorRequest
+    {
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
