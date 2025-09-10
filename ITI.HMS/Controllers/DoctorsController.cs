4f8c235e63deac26a8b08ff50f70fb0cf8cbb7f7
Amr Rizk <amrelseady65@gmail.com>
2025-09-10 22:05:36 +0300

    init project

---
+++ ITI.HMS/Controllers/DoctorsController.cs
@@ -0,0 +1,58 @@
+﻿using ITI.HMS.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.HMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly static List<Doctor> Doctors = new List<Doctor>
+        {
+            new Doctor
+            {
+                Id=1,
+                Name="Dr. John Smith",
+                Specialty="Cardiology",
+
+            },
+            new Doctor
+            {
+                Id=2,
+                Name="Dr. Emily Johnson",
+                Specialty="Neurology",
+
+            },
+            new Doctor
+            {
+                Id=3,
+                Name="Dr. Michael Brown",
+                Specialty="Pediatrics",
+
+            },
+
+        };
+
+        //host(domain)/api/Doctors
+
+        [HttpGet]
+        public List<Doctor> Get()
+        {
+            return Doctors;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Doctor> Get(int id)
+        {
+            if (id <=0)
+                return BadRequest();
+
+            var doctor = Doctors.Where(d => d.Id == id).FirstOrDefault();
+            if (doctor == null)
+                return NotFound($"Doctor with id {id} not found");
+            return Ok(doctor);
+        }
+
+    }
+}
---
+++ ITI.HMS/Models/Doctor.cs
@@ -0,0 +1,12 @@
+﻿namespace ITI.HMS.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
