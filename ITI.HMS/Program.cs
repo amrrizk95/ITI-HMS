7453d21200f671f9dc5077cc6e7cb5a4595f43b0
Amr Rizk <amrelseady65@gmail.com>
2025-09-17 21:04:38 +0300

    task/Refacor_doctor_controller : refactor doctorController and using services and repositories

--- ITI.HMS/Program.cs
+++ ITI.HMS/Program.cs
@@ -1,3 +1,8 @@
+using ITI.HMS.Repositories;
+using ITI.HMS.Repositories.Interfaces;
+using ITI.HMS.Services;
+using ITI.HMS.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>(); //register service / resolving dependency injection
+builder.Services.AddScoped<IDoctorService, DoctorService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ ITI.HMS/Services/DoctorService.cs
@@ -0,0 +1,38 @@
+﻿using ITI.HMS.Models;
+using ITI.HMS.Repositories;
+using ITI.HMS.Repositories.Interfaces;
+using ITI.HMS.Requestes;
+using ITI.HMS.Services.Interfaces;
+
+namespace ITI.HMS.Services
+{
+    public class DoctorService: IDoctorService
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public void CreateDoctor(CreatDoctorRequest doctor)
+        {
+            // some logic before adding docotor to db
+
+            ///logic 
+            ///add to database 
+            _doctorRepository.Add(doctor);
+        }
+
+        public List<Doctor> Get()
+        {
+            return _doctorRepository.Get();
+        }
+
+        public Doctor GetById(int id)
+        {
+            // repository service
+            var doctor = _doctorRepository.GetById(id);
+            return doctor;
+        }
+    }
+}
